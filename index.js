442510169fde922d3d983513c28cf8b993b61e32
Jashwanth-cyber <2024eb01692@online.bits-pilani.ac.in>
2025-06-03 11:24:11 +0530

    updated url

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const app = express();
 const cors = require("cors");
 
 app.use(cors({
-    origin: ["http://localhost:5173","https://csa-client-tau.vercel.app/"],
+    origin: ["http://localhost:5173","https://csa-client-tau.vercel.app"],
     credentials: true,
 }));
 
